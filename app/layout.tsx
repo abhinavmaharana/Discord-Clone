9a172c247fef6adab8f767a7d2bb96f8fcfe0165
abhinavmaharana <abhinavmaharana.004@gmail.com>
2023-09-13 10:39:02 +0530

    ✅ Added chat messages and socket

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { SocketProvider } from '@/components/providers/socket-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -30,9 +32,13 @@ export default function RootLayout({
             defaultTheme='dark'
             enableSystem={false}
             storageKey='discord-theme'
-          >
-            <ModalProvider />
-            {children}
+          > 
+            <SocketProvider>
+              <ModalProvider />
+                <QueryProvider>
+                  {children}
+                </QueryProvider>
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
